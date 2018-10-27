068a5a9b2159141988dd85b255f29a7124bc6d7e
Frank Hang <frankzhang123@gmail.com>
2018-10-27 12:45:13 +0800

    Parse cpp files with -f flag

--- main.go
+++ main.go
@@ -4,7 +4,11 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
+	"flag"
+	"fmt"
+	"io"
 	"os"
 )
 
@@ -16,9 +20,35 @@ void A() {  // Just an example
 
 `
 
+func parseArgs() (string, bool) {
+	var filename string
+	flag.StringVar(&filename, "f", "", "Specify the file name to be compiled")
+	flag.Parse()
+
+	if filename == "" {
+		fmt.Printf("filename=%s\n", filename)
+		return filename, false
+	}
+	fmt.Printf("filename=%s\n", filename)
+	return filename, true
+}
+
 func main() {
-	yyDebug = 10
+	yyDebug = 3
 	yyErrorVerbose = true
-	l := newLexer(bytes.NewBufferString(src), os.Stdout, "file.name")
+	var reader io.Reader
+	var name string
+	if filename, ok := parseArgs(); ok {
+		if fp, err := os.Open(filename); err == nil {
+			reader = bufio.NewReader(fp)
+			name = "file.name"
+		} else {
+			fmt.Fprintln(os.Stderr, err.Error())
+		}
+	} else {
+		reader = bytes.NewBufferString(src)
+		name = "file.name"
+	}
+	l := newLexer(reader, os.Stdout, name)
 	yyParse(l)
 }
---
+++ tests/test_01.cpp
@@ -0,0 +1,8 @@
+int a = 1;
+int main(int argv) {
+    return a + 1;
+}
+//Help me
+/*
+ Multiple line comment
+*/
